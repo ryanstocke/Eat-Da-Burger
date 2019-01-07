517752793afcd6d029ec18cb707776f5661964fa
ryanstocke <ryanstocke@gmail.com>
2019-01-06 19:09:21 -0700

    finalizing the put

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,31 +2,34 @@ var router = require("express").Router()
 
 var burger = require("../models/burger.js");
 
-router.get("/", function(req, res) {
-  
-    burger.all(function(result) {
-   
-      var hbsObject = {
-        burgers: result
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
-  });
+router.get("/", function (req, res) {
 
-  router.post('/burgers/create', function(req, res) {
-    burger.new('burger_name', req.body.burger_name, function(result) {
-      res.redirect('/');
-      console.log(result);
-    });
-  });
+  burger.all(function (result) {
 
-router.put('/burgers/update', function(req, res) {
-  burger.update(req.body.id, function(result) {
+    var hbsObject = {
+      burgers: result
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.put('/burgers/update', function (req, res) {
+  burger.update(req.body.burger_id, function (result) {
+    console.log(result);
+    res.redirect('/');
+    
+  });
+});
+
+router.post('/burgers/create', function (req, res) {
+  burger.new('burger_name', req.body.burger_name, function (result) {
     res.redirect('/');
     console.log(result);
   });
 });
+
+
 
 
 
