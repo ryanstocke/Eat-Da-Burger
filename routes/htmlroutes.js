57ab0f25078388d2b973f97f1604ec92b46a3f61
ryanstocke <ryanstocke@gmail.com>
2019-01-06 18:46:09 -0700

    post working, fixing update

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -14,14 +14,14 @@ router.get("/", function(req, res) {
     });
   });
 
-router.post('/', function(req, res) {
-  burger.new('burger_name', req.body.burger, function(result) {
-    res.redirect('/');
-    console.log(result);
+  router.post('/burgers/create', function(req, res) {
+    burger.new('burger_name', req.body.burger_name, function(result) {
+      res.redirect('/');
+      console.log(result);
+    });
   });
-});
 
-router.put('/', function(req, res) {
+router.put('/burgers/update', function(req, res) {
   burger.update(req.body.id, function(result) {
     res.redirect('/');
     console.log(result);
