e0d474a4eed6c5d3d7f5d05a275e4b6e30380822
ryanstocke <ryanstocke@gmail.com>
2019-01-08 01:28:15 -0700

    working

--- config/connection.js
+++ config/connection.js
@@ -12,7 +12,7 @@ if (process.env.JAWSDB_URL) {
         host: 'localhost',
         user: 'root',
         password: 'root',
-        database: 'burgers'
+        database: 'burgers_db'
     })
 };
 
